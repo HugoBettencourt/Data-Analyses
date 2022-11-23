d2229109256f5f8a1ed0c4ce833b7c055cad386f
HugoBettencourt <hugo.bettencourt95@gmail.com>
2022-11-23 17:25:31 +0000

    Added more changes

--- Portugal Wine/wine_analysis.R
+++ Portugal Wine/wine_analysis.R
@@ -7,6 +7,7 @@ setwd("C:/Users/hugob/Desktop/Personal Projects/DataAnalyses/Portugal Wine")
 # Install necessary packages
 library(ggplot2)
 library(car)
+library(MASS)
 
 # Load data - used the second version due to ; separator on csv file
 df<-read.csv2("data/wine.csv")
@@ -18,4 +19,12 @@ df[,c(1:11)]<-sapply(df[,c(1:11)], as.numeric)
 # variable
 model<-lm(quality ~ ., data=df)
 
-model2<-step(lm(quality ~ 1, df), scope=list(lower=~1, upper=~fixed_acidity+volatile_acidity+citric_acid+residual_sugar+chlorides+free_sulfur_dioxide+total_sulfur_dioxide+pH+sulphates+alcohol), direction="forward")
+# Applies the stepwise regression process to decide which
+# predictors to use in the final model
+step_model<-stepAIC(model, direction="both", trace=FALSE)
+
+# Added regression diagnostic to the step_model
+par(mfrow=c(1,2), oma = c(3,2,3,2) + 0.1, mar = c(1,1,1,1) + 0.1)
+truehist(residuals(step_model), h = 0.25, col="slategray3")
+qqPlot(residuals(step_model), pch=19, col="darkblue", cex=0.6)
+mtext("Distribution of Residuals", outer=T, side=1, line = 2)
